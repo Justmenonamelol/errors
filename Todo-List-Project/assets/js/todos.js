659bc6246470b63e46fe5b266adc6082b120fc28
GBadmin <33600866+Justmenonamelol@users.noreply.github.com>
2020-06-13 21:57:18 +0100

    Add files via upload

--- Todo-List-Project/assets/js/todos.js
+++ Todo-List-Project/assets/js/todos.js
@@ -1,20 +1,29 @@
+let ulParent = document.querySelector("ul");
 let list = document.querySelectorAll("li");
+let takeOut = document.querySelectorAll("span");
+let input = document.querySelector("input[type='text']");
+
+function logKey(e) {
+	if (e.which === 13) {
+		let inputValue = this.value;
+		console.log(inputValue);
+		ulParent.append(inputValue);
+	}
+}
 
 //loop through list
 for (let i = 0; i < list.length; i++) {
 	list[i].addEventListener("click", function () {
-		// list[i].classList.toggle(".completed");
-		console.log(list[i].style.color);
-
-		if (list[i].style.color === "") {
-			list[i].style.color = "rgb(128, 128, 128)";
-			list[i].style.textDecoration = "line-through";
-		} else if (list[i].style.color === "rgb(0, 0, 0)") {
-			list[i].style.color = "rgb(128, 128, 128)";
-			list[i].style.textDecoration = "line-through";
-		} else {
-			list[i].style.color = "rgb(0, 0, 0)";
-			list[i].style.textDecoration = "none";
-		}
+		list[i].classList.toggle("completed");
 	});
 }
+//loop through x
+for (let i = 0; i < takeOut.length; i++) {
+	takeOut[i].addEventListener("click", function (e) {
+		e.stopPropagation();
+		list[i].classList.add("fadeOut");
+		this.parentNode.remove();
+	});
+}
+//input adding values to todo list
+input.addEventListener("keypress", logKey);
