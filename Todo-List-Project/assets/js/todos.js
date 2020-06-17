713b513406755374658171dd567cd55cd368b78f
GBadmin <33600866+Justmenonamelol@users.noreply.github.com>
2020-06-17 12:34:17 +0100

    Add files via upload

--- Todo-List-Project/assets/js/todos.js
+++ Todo-List-Project/assets/js/todos.js
@@ -3,20 +3,49 @@ let list = document.querySelectorAll("li");
 let takeOut = document.querySelectorAll("span");
 let input = document.querySelector("input[type='text']");
 
+//input adding values to todo list
+input.addEventListener("keypress", logKey);
+
 function logKey(e) {
 	if (e.which === 13) {
 		let inputValue = this.value;
+		this.value = "";
 		console.log(inputValue);
-		ulParent.append(inputValue);
+		ulParent.insertAdjacentHTML(
+			"beforeend",
+			`<li><span>X</span> ` + inputValue + `</li>`
+		);
 	}
 }
-
-//loop through list
-for (let i = 0; i < list.length; i++) {
-	list[i].addEventListener("click", function () {
-		list[i].classList.toggle("completed");
+//loop through input
+for (let i = 0; i < input.length; i++) {
+	input.addEventListener("click", function (e) {
+		const target = e.target;
+		if (target.matches("input")) {
+			input[i].classList.toggle("completed");
+		} else {
+			console.log("empty");
+		}
 	});
 }
+//loop through list
+// for (let i = 0; i < list.length; i++) {
+// 	list[i].addEventListener("click", function () {
+// 		list[i].classList.toggle("completed");
+// 	});
+// }
+
+for (let i = 0; i < list.length; i++) {
+	list[i].addEventListener("click", function (e) {
+		console.log(list[i].style.color);
+		if (list[i].style.color === "") {
+			list[i].classList.toggle("completed");
+		} else {
+			console.log("empty");
+		}
+	});
+}
+
 //loop through x
 for (let i = 0; i < takeOut.length; i++) {
 	takeOut[i].addEventListener("click", function (e) {
@@ -25,5 +54,3 @@ for (let i = 0; i < takeOut.length; i++) {
 		this.parentNode.remove();
 	});
 }
-//input adding values to todo list
-input.addEventListener("keypress", logKey);
